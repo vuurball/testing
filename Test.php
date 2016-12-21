e2f9b3da173525135985a899c0b20aacb9441577
vuurball <oneindigedroom@gmail.com>
2016-12-21 21:35:37 +0200

    Update Test.php

--- Test.php
+++ Test.php
@@ -1,27 +1,9 @@
 <?php
-/*
+
 class Test extends PHPUnit_Framework_TestCase
 {
     public function testOnePlusOne() 
     {
         $this->assertEquals(1+1,2);
-    }
-}
-*/
-use PHPUnit\Framework\TestCase;
-
-class Test extends TestCase
-{
-    public function testPushAndPop()
-    {
-        $stack = [];
-        $this->assertEquals(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
     }
 }
